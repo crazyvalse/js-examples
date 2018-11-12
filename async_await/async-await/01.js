c3070b8786fdeee6747b00c6e7ff08d051b0eba9
IsaacGu <st.isaac.gu@me.com>
2018-11-12 18:36:58 +0800

    koa examples

--- async_await/async-await/01.js
+++ async_await/async-await/01.js
@@ -1,15 +1,11 @@
+async function f () {
+	return 1;
+}
 
-	async function f() {
-		return 1;
-	}
+f().then(console.info); // 1
 
-	f().then(console.info); // 1
+function _f () {
+	return Promise.resolve(1);
+}
 
-	function _f() {
-		return Promise.resolve(1);
-	}
-
-	_f().then(console.info); // 1
-
-
-
+_f().then(console.info); // 1
